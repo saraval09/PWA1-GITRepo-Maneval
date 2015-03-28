f9e6f2dafe8de34ba7a98a7d8e8983d4f703deeb
saraval09 <saramaneval@Saras-MacBook-Pro.local>
2015-03-27 21:32:35 -0400

    Week 4 assignment
    Week 4 assignment

---
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -0,0 +1,23 @@
+/**
+ Sara Maneval
+ 3/27/2015
+ Analyze Objects
+ */
+
+//Create a container called names and have  5 different names of people stored inside
+
+// Ex: var names= ["Eric", "Sara", "Rick","Jill","Kira"];
+
+//Create a for loop of 3 objects that makes calls to the person.js file and store it in an array called people
+
+//Randomly generate a name out of the names array by doing a random number between 0 and 4 to indicate which index the name is in
+
+//Randomly generate a number for the row do not hard code the number for the row
+
+//Create a function and have it output the persons name and job on the page
+
+//No duplicate names are allowed
+
+//Set the interval to 30 times a second
+//ex: setInterval(runUpdate, 1000/30);
+
---
+++ Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -0,0 +1,16 @@
+/**
+ Sara Maneval
+ 3/27/2015
+ Analyze Objects
+ */
+
+
+//Connect this page to the main.js page
+
+//Create two variables for the person object, jobs and actions
+//ex: Person.jobs=["artist","nurse", "mechanic"];
+//ex: Person.actions=["eating","working", "sleeping"];
+
+
+//Create four properties : name, action, job, and row
+
